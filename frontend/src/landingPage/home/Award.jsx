26f2ae8f86a0993ed5db5c2fdc3fd3d11e341043
whorajtripathi <rajsanjaykumartripathi@gmail.com>
2026-08-12 15:50:20 +0530

    Added the HeroSection and Award into the HomePage(Frontend)

--- frontend/src/landingPage/home/Award.jsx
+++ frontend/src/landingPage/home/Award.jsx
@@ -3,7 +3,47 @@
 function Award(){
     return(
         <>
-            <h1>Award</h1>
+            <div className="container p-5 mt-3">
+                <div className="row">
+
+                    <div className="col-6">
+                        <img src="images/largestBroker.svg" alt="largestBroker" style={{ width: "80%" }}/>
+                    </div>
+
+
+                    <div className="col-6 mt-3">
+                        <h1>Largest stock broker in India</h1>
+                        <p>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                        
+                        <div className="row mt-5 mb-5">
+                            <div className="col-6">
+                                <ul>
+                                    <li className="mb-1">Futures and Options</li>
+                                    <li className="mb-1">Commodity derivatives</li>
+                                    <li className="mb-1">Currency derivatives</li>
+                                </ul>
+                            </div>
+                            <div className="col-6">
+                                <ul>
+                                    <li className="mb-1">Stocks & IPOS</li>
+                                    <li className="mb-1">Direct mutual funds</li>
+                                    <li className="mb-1">Bonds and Go</li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <img
+                                src="images/pressLogos.png"
+                                alt="pressLogos"
+                                style={{ width: "100%" }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 }
--- frontend/src/landingPage/home/Hero.jsx
+++ frontend/src/landingPage/home/Hero.jsx
@@ -3,7 +3,14 @@
 function Hero(){
     return(
         <>
-            <h1>Hero</h1>
+            <div className="container p-5">
+                <div className="row text-center">
+                    <img src="images/homeHero.png" alt="Hero IMG" className="mb-5" height={"550px"} />
+                    <h1 className="mt-2">Invest In Everything</h1>
+                    <p>Online platform to invest in stocks, derivatives, mutual, funds and more</p>
+                    <button className="p-2 btn btn-primary" style={{width:"25%",margin:"0 auto"}}>SignUp!</button>
+                </div>
+            </div>
         </>
     );
 }
